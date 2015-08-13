fc0b38d3397c77e7cc83d46114edbe16bd43b91a
Charles Hamilton <HudsonWerks@users.noreply.github.com>
2015-08-13 12:16:25 -0400

    Update epoch-time.sh

--- epoch-time.sh
+++ epoch-time.sh
@@ -1,12 +1,22 @@
 # Epoch time in milliseconds
 # CTRL-C TO STOP
 # WRITTEN BY CHARLES HAMILTON
-# Simple script for showing the epoch time on a BeagleBone Black running Debian
+# Simple script for showing the epoch time at 10-second intervals on a BeagleBone Black running Debian
 
 #!/bin/bash
+
+# Resets the RTC from  the system clock
+sudo hwclock --systohc
+sudo hwclock --show
+
+# Runs command for determining epoch time
 for (( ; ; ))
 do
         echo -n "Since Epoch [in milliseconds]: "
-        cat /sys/class/rtc/rtc0/since_epoch | sed 's/...$//'
+# OPTION 1 command. Comment out if running OPTION 2.
+        echo $(($(date +%s%N)/1000000))
+
+## OPTION 2 command. Uncomment to run this version and comment out OPTION 1.
+#        cat /sys/class/rtc/rtc0/since_epoch | sed 's/...$//'
         sleep 10
 done
