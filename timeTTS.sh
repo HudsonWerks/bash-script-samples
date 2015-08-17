f029311806472df7098905bffee6008722fce77b
Charles Hamilton <HudsonWerks@users.noreply.github.com>
2015-08-17 16:26:58 -0400

    Update timeTTS.sh

--- timeTTS.sh
+++ timeTTS.sh
@@ -2,6 +2,7 @@
 NOW=$(date +"Ground Control time is %M minutes past %l %p U T C")
 echo $NOW
 pico2wave -w GroundControlTime.wav "$NOW"
+aplay GroundControlTime.wav
 #lame GroundControlTime.wav GroundControlTime.mp3
-mplayer -ao alsa:device=hw=0 GroundControlTime.wav
-rm temporaryTime.wav.*
+#mplayer -ao alsa:device=hw=0 GroundControlTime.wav
+rm GroundControlTime.wav.*
