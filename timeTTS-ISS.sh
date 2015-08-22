ddbb466ecb0946bb61ab03de2e6dbacf179031fc
Charles Hamilton <HudsonWerks@users.noreply.github.com>
2015-08-22 00:11:59 -0400

    Update timeTTS-ISS.sh

--- timeTTS-ISS.sh
+++ timeTTS-ISS.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# WRITTEN BY CHARLES HAMILTON
+# Script for showing the current local time and the current distance from a given coordinate for the International Space Station. Requires Python script ISS-current-distancy.py.
+
 ISS=$(sudo python /home/debian/ISS-current-distance.py)
 NOW=$(date +"Ground Control time is %M minutes past %l %p U T C with current distance to the International Space Station  $ISS miles")
 echo $NOW
